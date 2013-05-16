6c5e5eab5d3d982a7a2d6f65b3a800dd144fac99
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-05-16 15:51:28 +0800

    add python template file

---
+++ templates/template.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+""" %HERE%
+"""
+# Import built-in modules
+
+# Import third-party modules
+
+# Import own modules
+
+__author__ = %USER%
+__copyright__ = "Copyright © %YEAR%. All rights reserved."
+
+__maintainer__ = %USER%
+__email__ = %MAIL%
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+# vim:fenc=utf-8
